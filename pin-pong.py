398e71fe2609307817b060adcceb59bd02f3f6b4
lunrik1132 <prvlad007@gmail.com>
2022-05-04 20:13:08 +0300

    second commit

--- pin-pong.py
+++ pin-pong.py
@@ -1,5 +1,6 @@
 from pygame import *
 from random import randint, choice
+import time as tm
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed_x, player_speed_y, move_keys):
@@ -18,33 +19,28 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def move(self):
         keys = key.get_pressed()
-        if keys[self.move_keys[0]]:
-            self.rect.y -= self.speed_y
-        if keys[self.move_keys[1]]:
-            self.rect.y += self.speed_y
-
+        if keys[self.move_keys[0]] and self.rect.y > 0:
+            self.rect.y -= self.speed_y *dt/15
+        if keys[self.move_keys[1]] and self.rect.y < 350:
+            self.rect.y += self.speed_y *dt/15
+        
 class Ball(GameSprite):
     def move(self):
-        self.rect.y += self.speed_y
-        self.rect.x += self.speed_x
-        if self.rect.y == win_height-30 or self.rect.y == 0:
+        global timing
+        self.rect.y += self.speed_y *dt/15
+        self.rect.x += self.speed_x *dt/15
+        if self.rect.y >= win_height-30:
             self.speed_y = -self.speed_y
 
-        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
-            abc = randint(1,2)
-            print(abc)
-            if abc == 1:
-                self.speed_y = -self.speed_y
-                self.speed_x = -self.speed_x
+        if self.rect.y <= 0:
+            self.speed_y = -self.speed_y
 
-            if abc == 2:
-                self.speed_x = -(self.speed_x *1.2)
+        if sprite.collide_rect(player1, ball) and self.speed_x < 0:
+            self.speed_x = -self.speed_x
 
-            if self.speed_y > max_speed:
-                self.speed_y = max_speed
+        if sprite.collide_rect(player2, ball) and self.speed_x > 0:
+            self.speed_x = -self.speed_x
 
-            if self.speed_x > max_speed:
-                self.speed_x = max_speed
 
 img_player1 = "player1.png"
 img_player2 = "player2.png"
@@ -59,9 +55,13 @@ win_height = 500
 display.set_caption("Pin-pong")
 window = display.set_mode((win_width, win_height))
 
-player1 = Player(img_player1, 20, 150, 15, 150, 10, 10, player1keys)
-player2 = Player(img_player2, 665, 150, 15, 150, 10, 10, player2keys)
-ball = Ball(img_ball, win_width/2, win_height/2, 30, 30, 10, 10, player1keys)
+player1 = Player(img_player1, 20, 150, 15, 150, 5, 5, player1keys)
+player2 = Player(img_player2, 665, 150, 15, 150, 5, 5, player2keys)
+ball = Ball(img_ball, win_width/2, win_height/2, 30, 30, 5, randint(-5,5), player1keys)
+
+clock = time.Clock()
+fps = 60
+timing = tm.time()
 
 finish = False
 run = True 
@@ -69,8 +69,9 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-
+            
     if not finish:
+        dt = clock.tick(fps)
         draw.rect(window, (200,255,255), Rect(0, 0, win_width, win_height))
 
         player1.move()
@@ -81,5 +82,4 @@ while run:
         player1.reset()
         ball.reset()
         display.update()
-
-    time.delay(50)
+    
